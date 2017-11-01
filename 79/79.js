cf174b9b6c2b1df052d498269797bf23af07174d
yherz <yherz18@gmail.com>
2017-10-31 21:22:34 -0400

    hw 79

---
+++ 79/79.js
@@ -0,0 +1,9 @@
+
+document.getElementById("submit").addEventListener("click", function(){
+    var textColor = document.getElementById('text-color').value;
+    var backgroundColor = document.getElementById('background-color').value;
+    document.body.style.color = textColor;
+    document.body.style.backgroundColor = backgroundColor;
+});
+
+
