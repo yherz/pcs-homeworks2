cd4e75021bdd3325bc01e4e949879b4f7cab4717
yherz <yherz18@gmail.com>
2017-11-01 20:04:32 -0400

    use strict

--- 79/79.js
+++ 79/79.js
@@ -1,5 +1,6 @@
 
-document.getElementById("submit").addEventListener("click", function(){
+document.getElementById("submit").addEventListener("click", function () {
+    "use strict";
     var textColor = document.getElementById('text-color').value;
     var backgroundColor = document.getElementById('background-color').value;
     document.body.style.color = textColor;
